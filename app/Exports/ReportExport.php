802a43134bf14ccd365e6bb911f5cf54d414799f
Mario Guzman PC <makroz@hotmail.com>
2022-12-06 17:20:10 -0400

    format the file xlsx

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -2,11 +2,14 @@
 
 namespace App\Exports;
 
-use App\Models\Report;
 use App\Models\User;
+use App\Models\Report;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class ReportExport implements FromCollection
+class ReportExport implements FromCollection, WithHeadings, WithColumnWidths
 {
 
   public $report;
@@ -24,6 +27,28 @@ class ReportExport implements FromCollection
    */
   public function collection()
   {
-    return User::whereBetween('birth_date', [$this->report->date_from, $this->report->date_to])->orderBy('birth_date', 'desc')->get();
+    return User::select(['id', 'name', 'email', 'birth_date'])->whereBetween('birth_date', [$this->report->date_from, $this->report->date_to])->orderBy('birth_date', 'asc')->get();
+  }
+
+  public function headings(): array
+  {
+    return [
+      ['REPORTE de usuarios que nacieron entre: ' . $this->report->date_from . ' y ' . $this->report->date_to], [
+        'ID',
+        'NAME',
+        'EMAIL',
+        'BIRTHDAY'
+      ]
+    ];
+  }
+
+  public function columnWidths(): array
+  {
+    return [
+      'A' => 7,
+      'B' => 40,
+      'C' => 40,
+      'D' => 12,
+    ];
   }
 }
